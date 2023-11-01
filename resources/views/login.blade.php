3406cefde2b0feb0173fc3e5962efbcf4b3a4304
Wd. Ananda Lesmono <nandalesmono@gmail.com>
2023-11-01 15:19:47 +0800

    Menambahkan project website dapur marie

---
+++ resources/views/login.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,300&family=Poppins&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="login-page">
+        <h1 class="login">Login</h1>
+        <form action="" method="">
+            <label for="email">Email</label><br>
+            <input class="input-user" type="text" id="email" name="email" required><br><br>
+        
+            <label for="password">Password</label><br>
+            <input class="input-user" type="password" id="password" name="password" required><br><br>
+            
+            <input class="btn-login" type="submit" value="Login">
+        </form>
+        <a href="SignUp.html" class="sign-up">Don't have an account? <b>Sign Up</b></a>
+    </div>
+
+    
+    <div class="img-logo">
+
+    </div>
+
+</body>
+</html>
---
+++ resources/views/signup.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,300&family=Poppins&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="login-page">
+        <h1 class="login">Sign Up</h1>
+        <form action="" method="">
+            <label for="username">Username</label><br>
+            <input class="input-user" type="text" id="username" name="username" required><br><br>
+        
+            <label for="username">Email</label><br>
+            <input class="input-user" type="text" id="username" name="username" required><br><br>
+
+            <label for="password">Password</label><br>
+            <input class="input-user" type="password" id="password" name="password" required><br><br>
+
+            <label for="username">Confirm Password</label><br>
+            <input class="input-user" type="text" id="username" name="username" required><br><br>
+            
+            <input class="btn-login" type="submit" value="Sign Up">
+        </form>
+        <a href="" class="sign-up">Or already have an account? <b>Login</b></a>
+    </div>
+
+    
+    <div class="img-logo">
+        
+    </div>
+
+</body>
+</html>
