114c4b147dc9e4eeea5fbe3291d35aab18f7f0e7
Wd. Ananda Lesmono <nandalesmono@gmail.com>
2023-11-06 23:33:30 +0800

    memperbaiki halaman orders dan memperbaiki struktur view

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg sticky-top">
+      <img src="image/dapur-marie-txt.jpeg" />
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="/home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/menu">Menu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search..."
+              aria-label="Search"
+            />
+            <button class="btn btn-search" type="submit">Login</button>
+          </form>
+          <i class="fa fa-user-circle-o fa-2x ps-3 pe-1"></i>
+          <span>dhyaunn</span>
+        </div>
+      </div>
+    </nav>
